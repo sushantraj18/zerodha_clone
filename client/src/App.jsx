c8fd312c95c6e09cd76d36f5a321f86839bc6bc8
Sushant Raj <codexsushant@gmail.com>
2025-03-08 21:50:21 +0530

    route setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import HomePage from './pages/home/HomePage'
+import SignupPage from './pages/signup/SignupPage'
+import AboutPage from './pages/about/AboutPage'
+import ProductsPage from './pages/products/ProductsPage'
+import PricingPage from './pages/pricing/PricingPage'
 function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<HomePage/>}/>
+      <Route path="/open-account" element={<SignupPage/>}/>
+      <Route path ="/about" element={<AboutPage/>}/>
+      <Route path="/products" element={<ProductsPage/>}/>
+      <Route path="/charges" element={<PricingPage/>}/>
+      <Route path="support" element={<SignupPage/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/home/HomePage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import HeroSection from './HeroSection'
+import Stats from './Stats'
+import Pricing from './Pricing'
+import Education from './Education'
+function HomePage() {
+  return (
+    <>
+    <HeroSection/>
+    <Stats/>
+    <Pricing/>
+    <Education/>
+
+    </>
+  )
+}
+
+export default HomePage
